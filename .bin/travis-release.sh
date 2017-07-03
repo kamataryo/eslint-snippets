d534ebc3a8465dc7fd62ce7ada1707fd409318b8
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2017-07-04 08:56:13 +0900

    Set build up

---
+++ .bin/travis-release.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -e
+
+if [ $TRAVIS != "true" ]; then
+  echo "deploying only from Travis CI environment."
+  exit 0
+fi
+
+if [ $TRAVIS_BRANCH != "master" ]; then
+  echo "not deploying from $TRAVIS_BRANCH."
+  exit 0
+fi
+
+git config user.name 'kamataryo@travis'
+git config user.email "kamataryo@users.noreply.github.com"
+git remote remove origin
+git remote add origin git@github.com:kamataryo/eslint-snippets.git
+git checkout master
+
+# Auto upgrade
+if [ $TRAVIS_EVENT_TYPE == "cron" ]; then
+  echo 'Auto-upgrade is performing.'
+
+  yarn upgrade
+  npm-check-updates -u
+
+  if [[ $(git --no-pager diff) != "" ]]; then
+    git add .
+    git commit -m "Upgrade package [made in travis cron]"
+    git push origin master
+  fi
+fi
+
+rm -r .git
+rm -r .bin
+rm -r snippets
+rm .gitignore
+rm .travis_rsa.enc
+rm .travis.yml
+rm image.gif
+rm yarn.lock
+
+apm publish patch
